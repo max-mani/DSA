5db80c74f50f12c3093435c944c8189afbc4315a
Manikandan M <131739276+max-mani@users.noreply.github.com>
2026-07-05 15:26:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (77.63%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+    int mp(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            default: return 1000;
+        }
+    }
     int romanToInt(string s) {
         int out = 0;
-        unordered_map<char, int> mp = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
         for (int i = 0; i < s.size() - 1; i++) {
-            if (mp[s[i]] < mp[s[i + 1]]) out -= mp[s[i]];
-            else out += mp[s[i]];
+            if (mp(s[i]) < mp(s[i + 1])) out -= mp(s[i]);
+            else out += mp(s[i]);
         }
-        return out + mp[s[s.size() - 1]];
+        return out + mp(s[s.size() - 1]);
     }
 };
