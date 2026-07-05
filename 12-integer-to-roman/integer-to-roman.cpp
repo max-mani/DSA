b4701f6bcf6913c3e360a05b1c37a0653bdbf58c
Manikandan M <131739276+max-mani@users.noreply.github.com>
2026-07-05 17:44:11 +0530

    Time: 6 ms (33.98%) | Memory: 12.2 MB (48.74%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> vs = {{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"}, {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"}, {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}};
-        string out = "";
-        for (auto i : vs) {
-            if (num == 0) {
-                break;
-            }
-            while (num >= i.first) {
-                num -= i.first;
-                out += i.second;
+        vector<int> value = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> symbol = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        string ans;
+        for (int i = 0; i < value.size(); i++) {
+            while (num >= value[i]) {
+                ans += symbol[i];
+                num -= value[i];
             }
         }
-        return out;
+        return ans;
     }
 };
