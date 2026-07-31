b8a4fb4f39b5bd1d4b98d229d1e7276ae04ba4c0
Manikandan M <131739276+max-mani@users.noreply.github.com>
2026-07-31 20:59:20 +0530

    Time: 18 ms (83.28%) | Memory: 17.6 MB (44.67%) - LeetSync

--- 149-max-points-on-a-line/max-points-on-a-line.cpp
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -6,15 +6,10 @@ public:
             unordered_map<double, int>out;
             for (int j = i + 1; j < points.size(); j++) {
                 double x = (double) (points[j][1] - points[i][1]) / (double) (points[j][0] - points[i][0]);
-                if (points[j][0] - points[i][0] == 0) {
-                    out[abs(x)]++;
-                } else {
-                    out[x]++;
-                }
+                if (points[j][0] - points[i][0] == 0) out[abs(x)]++;
+                else out[x]++;
             }
-            for (auto& it : out) {
-                maxi = max(maxi, it.second);
-            }
+            for (auto& it : out) maxi = max(maxi, it.second);
         }
         return maxi + 1;
     }
