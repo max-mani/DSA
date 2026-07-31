da9b18e0ee72f45fe7de41b3c5a41991bd5916bd
Manikandan M <131739276+max-mani@users.noreply.github.com>
2026-07-31 20:55:44 +0530

    Time: 19 ms (79.79%) | Memory: 17.6 MB (44.67%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int maxi = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            unordered_map<double, int>out;
+            for (int j = i + 1; j < points.size(); j++) {
+                double x = (double) (points[j][1] - points[i][1]) / (double) (points[j][0] - points[i][0]);
+                if (points[j][0] - points[i][0] == 0) {
+                    out[abs(x)]++;
+                } else {
+                    out[x]++;
+                }
+            }
+            for (auto& it : out) {
+                maxi = max(maxi, it.second);
+            }
+        }
+        return maxi + 1;
+    }
+};
