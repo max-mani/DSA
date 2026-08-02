0b5254bb6b2ebcd27dea090b99070627e9a12c0b
Manikandan M <131739276+max-mani@users.noreply.github.com>
2026-08-02 16:47:46 +0530

    Time: 10 ms (28.51%) | Memory: 19.5 MB (27.79%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<vector<int>>out(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) out[i][i] = piles[i];
+        for (int ch = 1; ch < n; ch++) {
+            int i = 0;
+            for (int j = ch; j < n; i++, j++) {
+                out[i][j] = max(piles[i] - out[i + 1][j], piles[j] - out[i][j - 1]);
+            }
+        }
+        return out[0][n - 1] > 0;
     }
 };
