43d256812fcc5274f57c22e370f37c3d14968c75
Manikandan M <131739276+max-mani@users.noreply.github.com>
2026-08-16 15:10:42 +0530

    Time: 3 ms (75.38%) | Memory: 131.1 MB (100.00%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int a = 0, b = 0, c = 0;
+        for (int x : stones) {
+            if (x % 3 == 0) a++;
+            else if (x % 3 == 1) b++;
+            else c++;
+        }
+        if (a % 2 == 0) return b && c;
+        return abs(b - c) > 2;
+    }
+};
